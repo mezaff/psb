2b1477bf91f5bca9166ad8a8634627f29376fffc
mezaff <mezafuwandhana@gmail.com>
2025-01-31 11:12:14 +0700

    database connection

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 $host = "localhost"; // Nama hostnya
-$username = "root"; // Username
-$password = ""; // Password (Isi jika menggunakan password)
+$username = "pondokng_psb"; // Username
+$password = "E&~#DQBtsd3E"; // Password (Isi jika menggunakan password)
 $database = "psb"; // Nama databasenya
 $connect = mysqli_connect($host, $username, $password, $database); // Koneksi ke MySQL
