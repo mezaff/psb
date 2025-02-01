c8fc5b297b177b8fc0ceca370dce05f40de256b3
mezaff <mezafuwandhana@gmail.com>
2025-02-01 08:46:10 +0700

    feat: buka tutup pendaftaran2

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,17 +1,17 @@
 <?php
 
 // HOSTING
-// $host = "localhost";
-// $username = "pondokng_psb";
-// $password = "f2yB{thXvzpM";
-// $database = "pondokng_psb";
-// $connect = mysqli_connect($host, $username, $password, $database);
+$host = "localhost";
+$username = "pondokng_psb";
+$password = "f2yB{thXvzpM";
+$database = "pondokng_psb";
+$connect = mysqli_connect($host, $username, $password, $database);
 
 
 
 // LOCAL
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "psb";
-$connect = mysqli_connect($host, $username, $password, $database);
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "psb";
+// $connect = mysqli_connect($host, $username, $password, $database);
