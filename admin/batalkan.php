060a2498fefe7e7d5258643dcfa1114d5d39427b
mezaff <mezafuwandhana@gmail.com>
2025-03-13 17:59:43 +0700

    fix: verifikation datas

--- admin/batalkan.php
+++ admin/batalkan.php
@@ -1,74 +1,74 @@
 <?php
 include "../koneksi.php";
 
-// Ambil data NIS yang dikirim oleh index melalui URL
-$id = $_GET['id'];
+// Ambil data ID yang dikirim melalui URL secara aman
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
-
-// Query untuk menampilkan data siswa berdasarkan NIS yang dikirim
-$query = "SELECT * FROM siswa WHERE id='" . $id . "'";
-$sql = mysqli_query($connect, $query); // Eksekusi/Jalankan query dari variabel $query
+// Query untuk menampilkan data siswa berdasarkan ID
+$query = "SELECT * FROM siswa WHERE id='$id'";
+$sql = mysqli_query($connect, $query);
 $data = mysqli_fetch_array($sql);
-// Load file koneksi.php
 
-$id = $id;
-$nik = $data['nik'];
-$nisn = $data['nisn'];
-$namapd = $data['namapd'];
-$jk = $data['jk'];
-$tempatlahirpd = $data['tempatlahirpd'];
-$tanggallahirpd = $data['tanggallahirpd'];
-$wapd = $data['wapd'];
-$desapd = $data['desapd'];
-$kecamatanpd = $data['kecamatanpd'];
-$kabupatenpd = $data['kabupatenpd'];
-$provinsipd = $data['provinsipd'];
-$kodepos = $data['kodepos'];
-$alamatpd = $data['alamatpd'];
-$jarak = $data['jarak'];
-$transportasi = $data['transportasi'];
-$waktu = $data['waktu'];
-$jenispd = $data['jenispd'];
-$jenjang = $data['jenjang'];
-$asalsekolah = $data['asalsekolah'];
-$kk = $data['kk'];
-$nikayah = $data['nikayah'];
-$namaayah = $data['namaayah'];
-$tempatlahirayah = $data['tempatlahirayah'];
-$tanggallahirayah = $data['tanggallahirayah'];
-$pendidikanayah = $data['pendidikanayah'];
-$pekerjaanayah = $data['pekerjaanayah'];
-$penghasilanayah = $data['penghasilanayah'];
-$nikibu = $data['nikibu'];
-$namaibu = $data['namaibu'];
-$tempatlahiribu = $data['tempatlahiribu'];
-$tanggallahiribu = $data['tanggallahiribu'];
-$pendidikanibu = $data['pendidikanibu'];
-$pekerjaanibu = $data['pekerjaanibu'];
-$penghasilanibu = $data['penghasilanibu'];
-$wawali = $data['wawali'];
-$prestasi = $data['prestasi'];
-$jaminan = $data['jaminan'];
-$nomorbantuan = $data['nomorbantuan'];
-$pkh = $data['pkh'];
-$bantuan = $data['bantuan'];
+if (!$data) {
+    echo "Data tidak ditemukan!";
+    exit;
+}
+
+// Pastikan semua nilai aman dari karakter khusus
+function escape($value) {
+    global $connect;
+    return mysqli_real_escape_string($connect, $value);
+}
+
+$nik = escape($data['nik']);
+$nisn = escape($data['nisn']);
+$namapd = escape($data['namapd']);
+$jk = escape($data['jk']);
+$tempatlahirpd = escape($data['tempatlahirpd']);
+$tanggallahirpd = escape($data['tanggallahirpd']);
+$wapd = escape($data['wapd']);
+$desapd = escape($data['desapd']);
+$kecamatanpd = escape($data['kecamatanpd']);
+$kabupatenpd = escape($data['kabupatenpd']);
+$provinsipd = escape($data['provinsipd']);
+$kodepos = escape($data['kodepos']);
+$alamatpd = escape($data['alamatpd']);
+$jenispd = escape($data['jenispd']);
+$jenjang = escape($data['jenjang']);
+$asalsekolah = escape($data['asalsekolah']);
+$kk = escape($data['kk']);
+$nikayah = escape($data['nikayah']);
+$namaayah = escape($data['namaayah']);
+$tempatlahirayah = escape($data['tempatlahirayah']);
+$tanggallahirayah = escape($data['tanggallahirayah']);
+$pendidikanayah = escape($data['pendidikanayah']);
+$pekerjaanayah = escape($data['pekerjaanayah']);
+$penghasilanayah = escape($data['penghasilanayah']);
+$nikibu = escape($data['nikibu']);
+$namaibu = escape($data['namaibu']);
+$tempatlahiribu = escape($data['tempatlahiribu']);
+$tanggallahiribu = escape($data['tanggallahiribu']);
+$pendidikanibu = escape($data['pendidikanibu']);
+$pekerjaanibu = escape($data['pekerjaanibu']);
+$penghasilanibu = escape($data['penghasilanibu']);
+$wawali = escape($data['wawali']);
+$prestasi = escape($data['prestasi']);
+$jaminan = escape($data['jaminan']);
+$nomorbantuan = escape($data['nomorbantuan']);
+$pkh = escape($data['pkh']);
+$bantuan = escape($data['bantuan']);
 $status = 1;
 $keterangan = 1;
 
-
-$query = "SELECT * FROM siswa WHERE id='" . $id . "'";
-$sql = mysqli_query($connect, $query); // Eksekusi/Jalankan query dari variabel $query
-$data = mysqli_fetch_array($sql); // Ambil data dari hasil eksekusi $sql
-
 // Proses ubah data ke Database
-$query = "UPDATE siswa SET jenjang='" . $jenjang . "', nik='" . $nik . "', nisn='" . $nisn . "', namapd='" . $namapd . "', jk='" . $jk . "', tempatlahirpd='" . $tempatlahirpd . "', tanggallahirpd='" . $tanggallahirpd . "', wapd='" . $wapd . "', desapd='" . $desapd . "', kecamatanpd='" . $kecamatanpd . "', kabupatenpd='" . $kabupatenpd . "', provinsipd='" . $provinsipd . "', kodepos='" . $kodepos . "', alamatpd='" . $alamatpd . "', jenispd='" . $jenispd . "', asalsekolah='" . $asalsekolah . "', kk='" . $kk . "', nikayah='" . $nikayah . "', namaayah='" . $namaayah . "', tempatlahirayah='" . $tempatlahirayah . "', tanggallahirayah='" . $tanggallahirayah . "' , pendidikanayah='" . $pendidikanayah . "', pekerjaanayah='" . $pekerjaanayah . "', penghasilanayah='" . $penghasilanayah . "', nikibu='" . $nikibu . "', namaibu='" . $namaibu . "', tempatlahiribu='" . $tempatlahiribu . "', tanggallahiribu='" . $tanggallahiribu . "' , pendidikanibu='" . $pendidikanibu . "', pekerjaanibu='" . $pekerjaanibu . "', penghasilanibu='" . $penghasilanibu . "', wawali='" . $wawali . "', prestasi='" . $prestasi . "', jaminan='" . $jaminan . "', nomorbantuan='" . $nomorbantuan . "', pkh='" . $pkh . "', bantuan='" . $bantuan . "', status='" . $status . "', keterangan='" . $keterangan . "' WHERE id='" . $id . "'";
-$sql = mysqli_query($connect, $query); // Eksekusi/ Jalankan query dari variabel $query
+$query = "UPDATE siswa SET jenjang='$jenjang', nik='$nik', nisn='$nisn', namapd='$namapd', jk='$jk', tempatlahirpd='$tempatlahirpd', tanggallahirpd='$tanggallahirpd', wapd='$wapd', desapd='$desapd', kecamatanpd='$kecamatanpd', kabupatenpd='$kabupatenpd', provinsipd='$provinsipd', kodepos='$kodepos', alamatpd='$alamatpd', jenispd='$jenispd', asalsekolah='$asalsekolah', kk='$kk', nikayah='$nikayah', namaayah='$namaayah', tempatlahirayah='$tempatlahirayah', tanggallahirayah='$tanggallahirayah', pendidikanayah='$pendidikanayah', pekerjaanayah='$pekerjaanayah', penghasilanayah='$penghasilanayah', nikibu='$nikibu', namaibu='$namaibu', tempatlahiribu='$tempatlahiribu', tanggallahiribu='$tanggallahiribu', pendidikanibu='$pendidikanibu', pekerjaanibu='$pekerjaanibu', penghasilanibu='$penghasilanibu', wawali='$wawali', prestasi='$prestasi', jaminan='$jaminan', nomorbantuan='$nomorbantuan', pkh='$pkh', bantuan='$bantuan', status='$status', keterangan='$keterangan' WHERE id='$id'";
 
-if ($sql) { // Cek jika proses simpan ke database sukses atau tidak
-	// Jika Sukses, Lakukan :
-	header("location: datasantri"); // Redirect ke halaman index
+$sql = mysqli_query($connect, $query);
+
+if ($sql) {
+    header("Location: datasantri"); // Redirect ke halaman index
 } else {
-	// Jika Gagal, Lakukan :
-	echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
-	echo "<br><a href='/santribaru/admin'>Kembali</a>";
+    echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
+    echo "<br>Error: " . mysqli_error($connect);
+    echo "<br><a href='/santribaru/admin'>Kembali</a>";
 }
--- admin/verifikasi.php
+++ admin/verifikasi.php
@@ -1,74 +1,71 @@
 <?php
 include "../koneksi.php";
 
-// Ambil data NIS yang dikirim oleh index melalui URL
-$id = $_GET['id'];
+// Ambil data ID yang dikirim melalui URL dan pastikan berupa angka
+$id = intval($_GET['id']);
 
-
-// Query untuk menampilkan data siswa berdasarkan NIS yang dikirim
-$query = "SELECT * FROM siswa WHERE id='" . $id . "'";
-$sql = mysqli_query($connect, $query); // Eksekusi/Jalankan query dari variabel $query
+// Query untuk menampilkan data siswa berdasarkan ID
+$query = "SELECT * FROM siswa WHERE id=$id";
+$sql = mysqli_query($connect, $query);
 $data = mysqli_fetch_array($sql);
-// Load file koneksi.php
 
-$id = $id;
-$nik = $data['nik'];
-$nisn = $data['nisn'];
-$namapd = $data['namapd'];
-$jk = $data['jk'];
-$tempatlahirpd = $data['tempatlahirpd'];
-$tanggallahirpd = $data['tanggallahirpd'];
-$wapd = $data['wapd'];
-$desapd = $data['desapd'];
-$kecamatanpd = $data['kecamatanpd'];
-$kabupatenpd = $data['kabupatenpd'];
-$provinsipd = $data['provinsipd'];
-$kodepos = $data['kodepos'];
-$alamatpd = $data['alamatpd'];
-$jarak = $data['jarak'];
-$transportasi = $data['transportasi'];
-$waktu = $data['waktu'];
-$jenispd = $data['jenispd'];
-$jenjang = $data['jenjang'];
-$asalsekolah = $data['asalsekolah'];
-$kk = $data['kk'];
-$nikayah = $data['nikayah'];
-$namaayah = $data['namaayah'];
-$tempatlahirayah = $data['tempatlahirayah'];
-$tanggallahirayah = $data['tanggallahirayah'];
-$pendidikanayah = $data['pendidikanayah'];
-$pekerjaanayah = $data['pekerjaanayah'];
-$penghasilanayah = $data['penghasilanayah'];
-$nikibu = $data['nikibu'];
-$namaibu = $data['namaibu'];
-$tempatlahiribu = $data['tempatlahiribu'];
-$tanggallahiribu = $data['tanggallahiribu'];
-$pendidikanibu = $data['pendidikanibu'];
-$pekerjaanibu = $data['pekerjaanibu'];
-$penghasilanibu = $data['penghasilanibu'];
-$wawali = $data['wawali'];
-$prestasi = $data['prestasi'];
-$jaminan = $data['jaminan'];
-$nomorbantuan = $data['nomorbantuan'];
-$pkh = $data['pkh'];
-$bantuan = $data['bantuan'];
+if (!$data) {
+    echo "Data tidak ditemukan.";
+    exit;
+}
+
+// Escape string untuk mencegah error SQL
+function escape($value, $conn) {
+    return mysqli_real_escape_string($conn, $value);
+}
+
+$nik = escape($data['nik'], $connect);
+$nisn = escape($data['nisn'], $connect);
+$namapd = escape($data['namapd'], $connect);
+$jk = escape($data['jk'], $connect);
+$tempatlahirpd = escape($data['tempatlahirpd'], $connect);
+$tanggallahirpd = escape($data['tanggallahirpd'], $connect);
+$wapd = escape($data['wapd'], $connect);
+$desapd = escape($data['desapd'], $connect);
+$kecamatanpd = escape($data['kecamatanpd'], $connect);
+$kabupatenpd = escape($data['kabupatenpd'], $connect);
+$provinsipd = escape($data['provinsipd'], $connect);
+$kodepos = escape($data['kodepos'], $connect);
+$alamatpd = escape($data['alamatpd'], $connect);
+$jenispd = escape($data['jenispd'], $connect);
+$jenjang = escape($data['jenjang'], $connect);
+$asalsekolah = escape($data['asalsekolah'], $connect);
+$kk = escape($data['kk'], $connect);
+$nikayah = escape($data['nikayah'], $connect);
+$namaayah = escape($data['namaayah'], $connect);
+$tempatlahirayah = escape($data['tempatlahirayah'], $connect);
+$tanggallahirayah = escape($data['tanggallahirayah'], $connect);
+$pendidikanayah = escape($data['pendidikanayah'], $connect);
+$pekerjaanayah = escape($data['pekerjaanayah'], $connect);
+$penghasilanayah = escape($data['penghasilanayah'], $connect);
+$nikibu = escape($data['nikibu'], $connect);
+$namaibu = escape($data['namaibu'], $connect);
+$tempatlahiribu = escape($data['tempatlahiribu'], $connect);
+$tanggallahiribu = escape($data['tanggallahiribu'], $connect);
+$pendidikanibu = escape($data['pendidikanibu'], $connect);
+$pekerjaanibu = escape($data['pekerjaanibu'], $connect);
+$penghasilanibu = escape($data['penghasilanibu'], $connect);
+$wawali = escape($data['wawali'], $connect);
+$prestasi = escape($data['prestasi'], $connect);
+$jaminan = escape($data['jaminan'], $connect);
+$nomorbantuan = escape($data['nomorbantuan'], $connect);
+$pkh = escape($data['pkh'], $connect);
+$bantuan = escape($data['bantuan'], $connect);
 $status = 2;
 $keterangan = 1;
 
+// Query UPDATE data siswa
+$query = "UPDATE siswa SET jenjang='$jenjang', nik='$nik', nisn='$nisn', namapd='$namapd', jk='$jk', tempatlahirpd='$tempatlahirpd', tanggallahirpd='$tanggallahirpd', wapd='$wapd', desapd='$desapd', kecamatanpd='$kecamatanpd', kabupatenpd='$kabupatenpd', provinsipd='$provinsipd', kodepos='$kodepos', alamatpd='$alamatpd', jenispd='$jenispd', asalsekolah='$asalsekolah', kk='$kk', nikayah='$nikayah', namaayah='$namaayah', tempatlahirayah='$tempatlahirayah', tanggallahirayah='$tanggallahirayah', pendidikanayah='$pendidikanayah', pekerjaanayah='$pekerjaanayah', penghasilanayah='$penghasilanayah', nikibu='$nikibu', namaibu='$namaibu', tempatlahiribu='$tempatlahiribu', tanggallahiribu='$tanggallahiribu', pendidikanibu='$pendidikanibu', pekerjaanibu='$pekerjaanibu', penghasilanibu='$penghasilanibu', wawali='$wawali', prestasi='$prestasi', jaminan='$jaminan', nomorbantuan='$nomorbantuan', pkh='$pkh', bantuan='$bantuan', status='$status', keterangan='$keterangan' WHERE id=$id";
 
-$query = "SELECT * FROM siswa WHERE id='" . $id . "'";
-$sql = mysqli_query($connect, $query); // Eksekusi/Jalankan query dari variabel $query
-$data = mysqli_fetch_array($sql); // Ambil data dari hasil eksekusi $sql
+$sql = mysqli_query($connect, $query);
 
-// Proses ubah data ke Database
-$query = "UPDATE siswa SET jenjang='" . $jenjang . "', nik='" . $nik . "', nisn='" . $nisn . "', namapd='" . $namapd . "', jk='" . $jk . "', tempatlahirpd='" . $tempatlahirpd . "', tanggallahirpd='" . $tanggallahirpd . "', wapd='" . $wapd . "', desapd='" . $desapd . "', kecamatanpd='" . $kecamatanpd . "', kabupatenpd='" . $kabupatenpd . "', provinsipd='" . $provinsipd . "', kodepos='" . $kodepos . "', alamatpd='" . $alamatpd . "', jenispd='" . $jenispd . "', asalsekolah='" . $asalsekolah . "', kk='" . $kk . "', nikayah='" . $nikayah . "', namaayah='" . $namaayah . "', tempatlahirayah='" . $tempatlahirayah . "', tanggallahirayah='" . $tanggallahirayah . "' , pendidikanayah='" . $pendidikanayah . "', pekerjaanayah='" . $pekerjaanayah . "', penghasilanayah='" . $penghasilanayah . "', nikibu='" . $nikibu . "', namaibu='" . $namaibu . "', tempatlahiribu='" . $tempatlahiribu . "', tanggallahiribu='" . $tanggallahiribu . "' , pendidikanibu='" . $pendidikanibu . "', pekerjaanibu='" . $pekerjaanibu . "', penghasilanibu='" . $penghasilanibu . "', wawali='" . $wawali . "', prestasi='" . $prestasi . "', jaminan='" . $jaminan . "', nomorbantuan='" . $nomorbantuan . "', pkh='" . $pkh . "', bantuan='" . $bantuan . "', status='" . $status . "', keterangan='" . $keterangan . "' WHERE id='" . $id . "'";
-$sql = mysqli_query($connect, $query); // Eksekusi/ Jalankan query dari variabel $query
-
-if ($sql) { // Cek jika proses simpan ke database sukses atau tidak
-	// Jika Sukses, Lakukan :
-	header("location: datasantri"); // Redirect ke halaman index
+if ($sql) {
+    header("Location: datasantri"); // Redirect jika sukses
 } else {
-	// Jika Gagal, Lakukan :
-	echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
-	echo "<br><a href='datasantri'>Kembali</a>";
+    echo "Error: " . mysqli_error($connect); // Tampilkan error jika gagal
 }
